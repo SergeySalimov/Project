8cc29006d8d2e4477fe6100e5d8d7bffa6363cef
SergeySalimov <salimov_s@tut.by>
2019-11-11 15:47:43 +0300

    feat (form-script.js) js work

--- js/form-script.js
+++ js/form-script.js
@@ -5,13 +5,13 @@ console.log(form);
 
 form.addEventListener('submit', (event) => {
   console.log('11111');
-  // userInput.typeOfProduct = form[0].value;
-  // userInput.widthOfProduct = form[1].value;
-  // userInput.heightOfProduct = form[2].value;
-  // userInput.firstName = form[3].value;
-  // userInput.phoneNumber = form[4].value;
-  // console.log(userInput);
-  // form.reset();
+  userInput.typeOfProduct = form[0].value;
+  userInput.widthOfProduct = form[1].value;
+  userInput.heightOfProduct = form[2].value;
+  userInput.firstName = form[3].value;
+  userInput.phoneNumber = form[4].value;
+  console.log(userInput);
+  form.reset();
 
 
   event.preventDefault();
