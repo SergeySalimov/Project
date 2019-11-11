effe50ce26f942f880b9af75ea1ec860f2066af6
SergeySalimov <salimov_s@tut.by>
2019-11-11 14:35:42 +0300

    feat (form-script.js) js work

---
+++ js/form-script.js
@@ -0,0 +1,21 @@
+// document.addEventListener('DOMContentLoaded', () => {
+const form = document.getElementById('calculate-price');
+const userInput = {};
+console.log(form);
+
+form.addEventListener('submit', (event) => {
+  console.log('11111');
+  // userInput.typeOfProduct = form[0].value;
+  // userInput.widthOfProduct = form[1].value;
+  // userInput.heightOfProduct = form[2].value;
+  // userInput.firstName = form[3].value;
+  // userInput.phoneNumber = form[4].value;
+  // console.log(userInput);
+  // form.reset();
+
+
+  event.preventDefault();
+});
+
+
+// });
